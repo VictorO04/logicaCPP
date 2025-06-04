cdd6040d9af2c4a08ec4612a81021afb8b7b9d12
Victor Ferreira <victorfoliveira0407@gmail.com>
2025-06-04 16:57:41 -0300

    ex3 ifelse

--- ifElse/3.cpp
+++ ifElse/3.cpp
@@ -3,4 +3,17 @@ using namespace std;
 
 int main() {
     int num;
+    cout << "Digite um numero: ";
+    cin >> num;
+
+    if (num % 2 == 0)
+    {
+        cout << "o numero " << num << " e par!";
+    }
+    else
+    {
+        cout << "o numero " << num << " e impar!";
+    }
+
+    return 0;
 }
