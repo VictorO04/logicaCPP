5d6fc2b02e9d377f25232a96fcfcb680ad695128
Victor Ferreira <victorfoliveira0407@gmail.com>
2025-06-04 18:41:59 -0300

    ex 4 if else

---
+++ ifElse/4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string usuario;
+    int senha;
+
+    cout << "Usuario: ";
+    cin >> usuario;
+    cout << "Senha: ";
+    cin >> senha;
+
+    if (usuario == "admin" && senha == 123456)
+    {
+        cout << "Seja bem vindo " << usuario;
+    }
+    else if (usuario != "admin")
+    {
+        cout << "Usuario incorreto";
+    }
+    else if (usuario == "admin" && senha != 123456)
+    {
+        cout << "Senha incorreta";
+    }
+    
+    return 0;
+}
