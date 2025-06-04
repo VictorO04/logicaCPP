d933144274cafbdb7e8f568c509472b1b15c7453
Victor Ferreira <victorfoliveira0407@gmail.com>
2025-06-04 18:55:12 -0300

    else if finalizado

---
+++ ifElse/5.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float nota;
+
+    cout << "Digite sua nota: ";
+    cin >> nota;
+
+    if (nota >= 90 && nota <= 100)
+    {
+        cout << "Excelente! Parabens!";
+    }
+    else if (nota >= 70 && nota < 90)
+    {
+        cout << "Bom trabalho!";
+    }
+    else if (nota >= 50 && nota < 70)
+    {
+        cout << "Regular. Precisa melhorar.";
+    }
+    else if (nota >= 0 && nota < 50)
+    {
+        cout << "Insuficiente. Estude mais!";
+    }
+    else
+    {
+        cout << "Valor invalido";
+    }
+    
+    return 0;
+}
