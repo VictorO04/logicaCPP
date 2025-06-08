7c7d2f30be474d637f29a20eed76d79eced96175
Victor Ferreira <victorfoliveira0407@gmail.com>
2025-06-08 16:59:45 -0300

    Teoria

---
+++ _Teoria/variaveis.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    
+    int inteiro = 0;
+    unsigned int semSinal = 0;
+
+    float meuFloat = 10.9f;
+    double meuDouble = 10.9;
+
+    bool meuBool = true;
+    char myChar = 't';
+    char meuArray[11] = "0123456789";
+    std::string minhaString = "Teste";
+
+    size_t meuSizeT = 0;
+
+    system("pause");
+    return 0;
+}
